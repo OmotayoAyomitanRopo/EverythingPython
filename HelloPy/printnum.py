1a8a07f90d7620daa9c5905e5f57f0253ff731e1
Ayormitan <Kolawoleabaz97@gmail.com>
2025-04-08 06:49:50 -0700

    Random signed number print

--- HelloPy/printnum.py
+++ HelloPy/printnum.py
@@ -1,3 +1,10 @@
-#!/usr/bin/python3
-number = 98
-print (f"{number} Battery street")
+#!/usr/bin/env python
+import random
+number = random.randint(-10, 10)
+if number > 0:
+	print("{} is positive".format(number))
+elif number == 0:
+	print("{} is zero".format(number))
+else:
+	print("{} is negative".format(number))
+
