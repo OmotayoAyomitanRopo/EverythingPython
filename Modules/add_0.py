ffc83e2d4cdf9458318e58d942837cbf147f559d
Ayormitan <Kolawoleabaz97@gmail.com>
2025-04-09 06:41:19 -0700

    Importing add module

---
+++ Modules/add_0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+def add(a, b):
+    """Addition function
+    Args:
+        a: first argument
+        b: Second argument passed
+
+        returns: the sum of a and b
+    """
+    return (a + b)
