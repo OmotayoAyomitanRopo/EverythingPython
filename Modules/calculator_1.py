c691ce8bf6d828e4bb00700dd7907119444c06cc
Ayormitan <Kolawoleabaz97@gmail.com>
2025-04-09 07:06:10 -0700

    Module does some calculations

---
+++ Modules/calculator_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+def add(a, b):
+    """
+        my addition function
+    """
+    return (a + b)
+
+def sub(a, b):
+    return (a - b)
+
+def mul(a, b):
+    return (a * b)
+
+def div(a, b):
+    return (a / b)
