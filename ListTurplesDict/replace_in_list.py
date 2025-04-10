74782fa4a6d6d0057dea4b1caee14d0a771f2ea1
Ayormitan <Kolawoleabaz97@gmail.com>
2025-04-10 11:17:56 -0700

    Creates a new list without modifying the old list

--- ListTurplesDict/replace_in_list.py
+++ ListTurplesDict/replace_in_list.py
@@ -4,6 +4,7 @@ def replace_in_list(my_list, idx, element):
         return my_list
     if idx >= len(my_list):
         return my_list
-
-    my_list[idx] = element
-    return my_list
+    
+    new_list = my_list[:] #store a copy of the original list
+    new_list[idx] = element # creatte a new list to include the new element
+    return new_list #return the newly created list
