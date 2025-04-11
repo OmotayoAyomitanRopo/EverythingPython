f63deceb3826bd180e37ac0ffccab950d0b7c308
Ayormitan <Kolawoleabaz97@gmail.com>
2025-04-11 06:03:24 -0700

    Find largest number

---
+++ HelloPy/9-Lmain.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+max_integer = __import__('find_biggest_int').max_integer
+
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
