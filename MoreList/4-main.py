059f95f479b336598aaf05356e15fa007a3a7c7c
Ayormitan <Kolawoleabaz97@gmail.com>
2025-04-15 12:54:37 -0700

    Returns only sementrical set element

---
+++ MoreList/4-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+only_diff_elements = __import__('Only_Oneset').only_diff_elements
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
