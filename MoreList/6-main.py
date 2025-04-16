de42da77c17e8ece756cda3187db5f784c5ab3e8
Ayormitan <Kolawoleabaz97@gmail.com>
2025-04-16 12:01:23 -0700

    Print order key - value

---
+++ MoreList/6-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+print_sorted_dictionary = __import__('OrderKeys').print_sorted_dictionary
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
