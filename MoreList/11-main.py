d096dd91441d556462fbbfa82817d391f445f2c9
Ayormitan <Kolawoleabaz97@gmail.com>
2025-04-17 11:38:50 -0700

    Maping a list

---
+++ MoreList/11-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+multiply_list_map = __import__('map').multiply_list_map
+
+my_list = [1, 2, 3, 4, 5]
+new_list = multiply_list_map(my_list, 4)
+
+print(my_list)
+print(new_list)
