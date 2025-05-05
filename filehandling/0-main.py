72f4ce192f832859932fe3f10f79cae7cc48ef40
Ayormitan <Kolawoleabaz97@gmail.com>
2025-05-05 09:24:16 -0700

    Function reads a text file

---
+++ filehandling/0-main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+read_file = __import__('readfile').read_file
+
+read_file("my_file_0.txt")
---
+++ filehandling/readfile.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+def read_file(filename=""):
+    with open("my_file_0.txt", encoding="utf8") as f:
+        print(f.read())
+
