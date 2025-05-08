69ce7b72db0bda0745ca15d1fa166fd8ef067538
Ayormitan <Kolawoleabaz97@gmail.com>
2025-05-08 13:00:56 -0700

    Added a dictionary method to square

--- OOPCircle/models/square.py
+++ OOPCircle/models/square.py
@@ -30,6 +30,14 @@ class Square(Rectangle):
             if hasattr(self, key):
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+        return {
+                'id': self.id,
+                'size': self.size,
+                'x': self.x,
+                'y': self.y
+            }
+
     def __str__(self):
         return f"[square] ({self.id}) ({self.x})/({self.y}) - ({self.size})"
 
