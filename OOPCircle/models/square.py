f64f36f29c32cc5a326e7dac08584f05aa38c0bf
Ayormitan <Kolawoleabaz97@gmail.com>
2025-05-08 06:15:32 -0700

    Updated square with public getter and setter with value validation

--- OOPCircle/models/square.py
+++ OOPCircle/models/square.py
@@ -7,7 +7,19 @@ class Square(Rectangle):
         self.size = size
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        # Validation for size setter
+        if not isinstance(value, int):
+            raise TypeError("size must be an int")
+        if value < 0:
+            raise ValueError("size must be greater than 0")
+        self.__size = value
+
     def __str__(self):
         return f"[square] ({self.id}) ({self.x})/({self.y}) - ({self.size})"
-
 
