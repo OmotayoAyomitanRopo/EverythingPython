98c3be50c97d4d4e457dbe9e38e4189ca98a35ce
Ayormitan <Kolawoleabaz97@gmail.com>
2025-05-08 13:55:50 -0700

    Writing the JSON representation to a fdile

--- OOPCircle/models/base.py
+++ OOPCircle/models/base.py
@@ -12,8 +12,21 @@ class Base:
             Base.__nb_objects += 1 # If id value is not provided increment with private class attribute
             self.id = Base.__nb_objects # Then assign the new value to the public instance attribute id
             
+    # Retruns the json string representation of a dict
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or not list_dictionaries :
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = f"{cls.__name__}.json"
+
+        if list_objs is not None:
+            obj_dicts = [obj.to_dictionary() for obj in list_objs]
+
+        json_string = cls.to_json_string(obj_dicts)
+
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(json_string)
